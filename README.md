855bd709d44a7bbadca770241778ca60f6372604
Thiago Locatelli <thiago.locatelli@gmail.com>
2019-10-05 09:44:46 -0400

    Added description and fixed properties

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 ## spring-cloud-zuul-server
+
+Simple docker container running Netflix Zuul Proxy. Spring Boot version 2.1.9-RELEASE
+
+default port: 8080
+
+```shell script
+docker run thiagolocatelli/spring-cloud-zuul-server
+```
+
+By default, the application does not register itself with Eureka server, to enable this feature the
+following environment variables need to be set:
+
+```shell script
+
+# enable discovery client
+EUREKA_CLIENT_ENABLED=true
+
+# set the service url for discovery client (default: http://localhost:8888/eureka)
+EUREKA_CLIENT_SERVICE_URL=http://localhost:8888/eureka
+```
+
+```shell script
+docker run -e EUREKA_CLIENT_ENABLED=true EUREKA_CLIENT_SERVICE_URL=http://localhost:8888/eureka thiagolocatelli/spring-cloud-zuul-serverß
+```
+
+If customizations are required for the Swagger UI, use these extra environment variables:
+
+```shell script
+INFO_APP_CONTACT_NAME
+INFO_APP_CONTACT_URL
+INFO_APP_CONTACT_EMAIL
+INFO_APP_NAME
+INFO_APP_DESCRIPTION
+INFO_APP_VERSIONß
+```
---
+++ src/main/java/com/github/thiagolocatelli/cloud/zuulserver/Swagger2Config.java
@@ -0,0 +1,69 @@
+package com.github.thiagolocatelli.cloud.zuulserver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public Docket api() {
+
+        Contact contact = new Contact(
+                this.environment.getProperty("info.app.contact.name"),
+                this.environment.getProperty("info.app.contact.url"),
+                this.environment.getProperty("info.app.contact.email"));
+
+        ApiInfo apiInfo = new ApiInfo(
+                this.environment.getProperty("info.app.name"),
+                this.environment.getProperty("info.app.description"),
+                this.environment.getProperty("info.app.version"),
+                null,
+                contact,
+                "MIT",
+                "https://opensource.org/licenses/MIT",
+                Collections.emptyList());
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(regex("/api.*|/actuator/.*"))
+                .build();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        // Allow anyone and anything access. Probably ok for Swagger spec
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        source.registerCorsConfiguration("/v2/api-docs", config);
+        return new CorsFilter(source);
+    }
+
+}
